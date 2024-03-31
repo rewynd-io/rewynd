e32bda82145ce587aeb5def26e16cd976ab937d3
kensand <git@kensand.net>
2024-03-30 21:14:07 -0400

    Pull out main function

--- rewynd-worker/src/main/kotlin/io/rewynd/worker/Main.kt
+++ rewynd-worker/src/main/kotlin/io/rewynd/worker/Main.kt
@@ -1,6 +1,5 @@
 package io.rewynd.worker
 
-import io.github.oshai.kotlinlogging.KotlinLogging
 import io.rewynd.common.cache.Cache
 import io.rewynd.common.cache.queue.getImageJobQueue
 import io.rewynd.common.cache.queue.getScanJobQueue
@@ -14,50 +13,52 @@ import io.rewynd.worker.schedule.ScheduleHandler
 import io.rewynd.worker.search.SearchHandler
 import io.rewynd.worker.stream.mkStreamJobHandler
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlin.time.Duration.Companion.minutes
 
-val config by lazy { WorkerConfig.fromConfig() }
-val cache by lazy { Cache.fromConfig(config.cache) }
-
-private val log by lazy { KotlinLogging.logger { } }
-
 fun main() =
     runBlocking(Dispatchers.IO) {
+        val config = WorkerConfig.fromConfig()
         val db = Database.fromConfig(config.database)
+        val cache = Cache.fromConfig(config.cache)
         db.init()
 
-        val searchHandler = SearchHandler(db)
-        val searchUpdateJob =
-            launch(Dispatchers.IO) {
-                while (true) {
-                    searchHandler.updateIndicies()
-                    delay(1.minutes)
-                }
-            }
-
-        val scanQueue = cache.getScanJobQueue()
-        val streamQueue = cache.getStreamJobQueue()
-        val imageQueue = cache.getImageJobQueue()
-        val searchQueue = cache.getSearchJobQueue()
-        val scheduleRefreshJobQueue = cache.getScheduleRefreshJobQueue()
-
-        val scanJobHandler = scanQueue.register(mkScanJobHandler(db), this)
-        val streamJobHandler = streamQueue.register(mkStreamJobHandler(cache), this)
-        val searchJobHandler = searchQueue.register(searchHandler.jobHander, this)
-        val imageJobHandlers = (0 until 100).map { imageQueue.register(mkImageJobHandler(cache), this) }
-        val scheduleJob = ScheduleHandler(db, cache, scanQueue, scheduleRefreshJobQueue).run(this)
-
-        (
-            listOf(
-                scanJobHandler,
-                streamJobHandler,
-                searchJobHandler,
-                searchUpdateJob,
-                scheduleJob,
-            ) + imageJobHandlers
-        ).joinAll()
+        runWorker(db, cache).joinAll()
     }
+
+suspend fun runWorker(db: Database, cache: Cache): List<Job> = coroutineScope {
+    val searchHandler = SearchHandler(db)
+    val searchUpdateJob = coroutineScope {
+        launch(Dispatchers.IO) {
+            while (true) {
+                searchHandler.updateIndicies()
+                delay(1.minutes)
+            }
+        }
+    }
+
+    val scanQueue = cache.getScanJobQueue()
+    val streamQueue = cache.getStreamJobQueue()
+    val imageQueue = cache.getImageJobQueue()
+    val searchQueue = cache.getSearchJobQueue()
+    val scheduleRefreshJobQueue = cache.getScheduleRefreshJobQueue()
+
+    val scanJobHandler = scanQueue.register(mkScanJobHandler(db), this)
+    val streamJobHandler = streamQueue.register(mkStreamJobHandler(cache), this)
+    val searchJobHandler = searchQueue.register(searchHandler.jobHander, this)
+    val imageJobHandlers = (0 until 100).map { imageQueue.register(mkImageJobHandler(cache), this) }
+    val scheduleJob = ScheduleHandler(db, cache, scanQueue, scheduleRefreshJobQueue).run(this)
+
+    listOf(
+        scanJobHandler,
+        streamJobHandler,
+        searchJobHandler,
+        searchUpdateJob,
+        scheduleJob,
+    ) + imageJobHandlers
+}
