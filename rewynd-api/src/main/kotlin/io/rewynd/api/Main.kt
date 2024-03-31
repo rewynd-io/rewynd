c41fca11fa1a4189034712a26fee6e83c9c207c3
kensand <git@kensand.net>
2024-03-31 10:55:39 -0400

    Fix main functions

--- rewynd-api/src/main/kotlin/io/rewynd/api/Main.kt
+++ rewynd-api/src/main/kotlin/io/rewynd/api/Main.kt
@@ -1,44 +1,9 @@
 package io.rewynd.api
 
-import io.github.oshai.kotlinlogging.KotlinLogging
-import io.ktor.http.HttpStatusCode
-import io.ktor.server.application.Application
-import io.ktor.server.application.install
 import io.ktor.server.cio.CIO
 import io.ktor.server.engine.embeddedServer
-import io.ktor.server.http.content.react
-import io.ktor.server.http.content.singlePageApplication
-import io.ktor.server.plugins.statuspages.StatusPages
-import io.ktor.server.request.httpMethod
-import io.ktor.server.request.path
-import io.ktor.server.response.respondText
-import io.ktor.server.routing.route
-import io.ktor.server.routing.routing
-import io.rewynd.api.controller.authRoutes
-import io.rewynd.api.controller.episodeRoutes
-import io.rewynd.api.controller.imageRoutes
-import io.rewynd.api.controller.libRoutes
-import io.rewynd.api.controller.progressRoutes
-import io.rewynd.api.controller.scheduleRoutes
-import io.rewynd.api.controller.searchRoutes
-import io.rewynd.api.controller.seasonRoutes
-import io.rewynd.api.controller.showRoutes
-import io.rewynd.api.controller.streamRoutes
-import io.rewynd.api.controller.userRoutes
-import io.rewynd.api.plugins.configureHTTP
-import io.rewynd.api.plugins.configureMonitoring
-import io.rewynd.api.plugins.configureSerialization
-import io.rewynd.api.plugins.configureSession
-import io.rewynd.api.plugins.mkAuthNPlugin
 import io.rewynd.common.cache.Cache
-import io.rewynd.common.cache.queue.getImageJobQueue
-import io.rewynd.common.cache.queue.getScanJobQueue
-import io.rewynd.common.cache.queue.getScheduleRefreshJobQueue
-import io.rewynd.common.cache.queue.getSearchJobQueue
-import io.rewynd.common.cache.queue.getStreamJobQueue
 import io.rewynd.common.database.Database
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 
@@ -48,12 +13,9 @@ fun main(): Unit =
         val db = Database.fromConfig()
         db.init()
 
-        runApi(db, cache).join()
+        runApi(db, cache)
     }
 
-suspend fun runApi(db: Database, cache: Cache) = coroutineScope {
-    launch {
-        embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = { module(db, cache) })
-            .start(wait = true)
-    }
-}
+fun runApi(db: Database, cache: Cache) =
+    embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = { module(db, cache) })
+        .start(wait = true)
--- rewynd-worker/src/main/kotlin/io/rewynd/worker/Main.kt
+++ rewynd-worker/src/main/kotlin/io/rewynd/worker/Main.kt
@@ -13,51 +13,53 @@ import io.rewynd.worker.schedule.ScheduleHandler
 import io.rewynd.worker.search.SearchHandler
 import io.rewynd.worker.stream.mkStreamJobHandler
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlin.time.Duration.Companion.minutes
 
-fun main() =
-    runBlocking(Dispatchers.IO) {
-        val cache = Cache.fromConfig()
-        val db = Database.fromConfig()
+
+fun main() {
+    val cache = Cache.fromConfig()
+    val db = Database.fromConfig()
+    runBlocking {
         db.init()
-
-        runWorker(db, cache).joinAll()
     }
-
-suspend fun runWorker(db: Database, cache: Cache): List<Job> = coroutineScope {
-    val searchHandler = SearchHandler(db)
-    val searchUpdateJob = coroutineScope {
-        launch(Dispatchers.IO) {
-            while (true) {
-                searchHandler.updateIndicies()
-                delay(1.minutes)
-            }
-        }
-    }
-
-    val scanQueue = cache.getScanJobQueue()
-    val streamQueue = cache.getStreamJobQueue()
-    val imageQueue = cache.getImageJobQueue()
-    val searchQueue = cache.getSearchJobQueue()
-    val scheduleRefreshJobQueue = cache.getScheduleRefreshJobQueue()
-
-    val scanJobHandler = scanQueue.register(mkScanJobHandler(db), this)
-    val streamJobHandler = streamQueue.register(mkStreamJobHandler(cache), this)
-    val searchJobHandler = searchQueue.register(searchHandler.jobHander, this)
-    val imageJobHandlers = (0 until 100).map { imageQueue.register(mkImageJobHandler(cache), this) }
-    val scheduleJob = ScheduleHandler(db, cache, scanQueue, scheduleRefreshJobQueue).run(this)
-
-    listOf(
-        scanJobHandler,
-        streamJobHandler,
-        searchJobHandler,
-        searchUpdateJob,
-        scheduleJob,
-    ) + imageJobHandlers
+    runWorker(db, cache)
 }
+
+fun runWorker(db: Database, cache: Cache) =
+    runBlocking(Dispatchers.IO) {
+        val searchHandler = SearchHandler(db)
+        val searchUpdateJob =
+            launch(Dispatchers.IO) {
+                while (true) {
+                    searchHandler.updateIndicies()
+                    delay(1.minutes)
+                }
+            }
+
+        val scanQueue = cache.getScanJobQueue()
+        val streamQueue = cache.getStreamJobQueue()
+        val imageQueue = cache.getImageJobQueue()
+        val searchQueue = cache.getSearchJobQueue()
+        val scheduleRefreshJobQueue = cache.getScheduleRefreshJobQueue()
+
+        val scanJobHandler = scanQueue.register(mkScanJobHandler(db), this)
+        val streamJobHandler = streamQueue.register(mkStreamJobHandler(cache), this)
+        val searchJobHandler = searchQueue.register(searchHandler.jobHander, this)
+        val imageJobHandlers =
+            (0 until 100).map { imageQueue.register(mkImageJobHandler(cache), this) }
+        val scheduleJob = ScheduleHandler(db, cache, scanQueue, scheduleRefreshJobQueue).run(this)
+
+        (
+                listOf(
+                    scanJobHandler,
+                    streamJobHandler,
+                    searchJobHandler,
+                    searchUpdateJob,
+                    scheduleJob,
+                ) + imageJobHandlers
+                ).joinAll()
+    }
