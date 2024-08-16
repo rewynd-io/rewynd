a64696b84a251ff6d5410e7a699b4ef64a4583e6
kensand <git@kensand.net>
2024-08-15 20:59:48 -0400

    Fix up BrowserNavigationActions

--- rewynd-android/src/main/kotlin/io/rewynd/android/browser/BrowserNavigationActions.kt
+++ rewynd-android/src/main/kotlin/io/rewynd/android/browser/BrowserNavigationActions.kt
@@ -55,68 +55,42 @@ interface IBrowserNavigationActions {
 
 class BrowserNavigationActions(private val navController: NavHostController) : IBrowserNavigationActions {
     override fun home() = navController.navigate(BrowserState.HomeState) {
-        popUpTo(BrowserState.HomeState) {
-            saveState = true
-        }
+        popUpTo(BrowserState.HomeState)
         launchSingleTop = true
-        restoreState = true
     }
 
     override fun search() = navController.navigate(BrowserState.SearchState) {
-        popUpTo(BrowserState.SearchState) {
-            saveState = true
-        }
+        popUpTo(BrowserState.SearchState)
         launchSingleTop = true
-        restoreState = true
     }
 
     override fun episode(episodeInfo: EpisodeInfo) = BrowserState.EpisodeState(episodeInfo).let {
         navController.navigate(it) {
-            popUpTo(it) {
-                saveState = true
-            }
-            launchSingleTop = true
-            restoreState = true
+            popUpTo(it)
         }
     }
 
     override fun library(library: Library) = BrowserState.LibraryState(library).let {
         navController.navigate(it) {
-            popUpTo(it) {
-                saveState = true
-            }
-            launchSingleTop = true
-            restoreState = true
+            popUpTo(it)
         }
     }
 
     override fun movie(movieInfo: MovieInfo) = BrowserState.MovieState(movieInfo).let {
         navController.navigate(it) {
-            popUpTo(it) {
-                saveState = true
-            }
-            launchSingleTop = true
-            restoreState = true
+            popUpTo(it)
         }
     }
 
     override fun show(showInfo: ShowInfo) = BrowserState.ShowState(showInfo).let {
         navController.navigate(it) {
-            popUpTo(it) {
-                saveState = true
-            }
-            launchSingleTop = true
-            restoreState = true
+            popUpTo(it)
         }
     }
 
     override fun season(seasonInfo: SeasonInfo) = BrowserState.SeasonState(seasonInfo).let {
         navController.navigate(it) {
-            popUpTo(it) {
-                saveState = true
-            }
-            launchSingleTop = true
-            restoreState = true
+            popUpTo(it)
         }
     }
 }
